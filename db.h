bd7dfa2e4111c6091c50ea9796d7f7f3f626ae70
barrystyle <barrystyle@westnet.com.au>
2023-08-24 00:05:43 +0800

    add cstdint for older compilers

--- db.h
+++ db.h
@@ -1,7 +1,13 @@
+#ifndef DB_H
+#define DB_H
+
 #include <stdio.h>
 #include <string.h>
 #include <fstream>
+#include <cstdint>
 
 bool exists_in_db(std::string& address);
 void return_exact_line(uint64_t linenum, std::string& address);
 void initdb();
+
+#endif // DB_H
